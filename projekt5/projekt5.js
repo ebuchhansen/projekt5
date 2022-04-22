4bbb4304862ca49d305af082d917aea7c85038f6
Ruskindshood <makk@makk.dk>
2022-04-22 14:32:31 +0200

    En masse
    Hel masse

--- projekt5/projekt5.js
+++ projekt5/projekt5.js
@@ -1,4 +1,4 @@
-
+// Kode - Emilie //
 let instagramTitle = document.getElementById("instagramtitle");
 instagramTitle.style.color = "#FD5A05";
 
@@ -23,3 +23,23 @@ while (i < 1) {
     i++;
 }
 document.getElementById("rydningtitle").innerHTML = text;
+
+// Kode - Magnus //
+
+const body = document.querySelector("body");
+const navbar = document.querySelector(".navbar");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+menuBtn.onclick = ()=>{
+  navbar.classList.add("show");
+  menuBtn.classList.add("hide");
+  body.classList.add("disabled");
+}
+cancelBtn.onclick = ()=>{
+  body.classList.remove("disabled");
+  navbar.classList.remove("show");
+  menuBtn.classList.remove("hide");
+}
+window.onscroll = ()=>{
+  this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+}

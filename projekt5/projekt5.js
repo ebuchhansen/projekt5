209660534381a22ed4f0b71fa95418aca5efd3ae
Emilie <96144802+ebuchhansen@users.noreply.github.com>
2022-04-22 11:29:33 +0200

    Emilie first code

---
+++ projekt5/projekt5.js
@@ -0,0 +1,27 @@
+
+// emilie start
+let instagramTitle = document.getElementById("instagramtitle");
+instagramTitle.style.color = "#FD5A05";
+
+let number = 1
+
+if (number > 0){
+    console.log("ifWorking")
+}
+else {
+    console.log("elseWorking")
+}
+
+var butik = {firstName:"@butik", lastName:"pulvis"};
+
+document.getElementById("instaid").innerHTML =
+butik.firstName + butik.lastName;
+
+let text = "";
+let i = 0;
+while (i < 1) {
+    text += "<h1>Hvordan forgår en rydning? ";
+    i++;
+}
+document.getElementById("rydningtitle").innerHTML = text;
+// emilie done
